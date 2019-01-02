82cd7a4f3b1fcccf3e9b12cc1a8a4820747399cf
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-01-02 16:40:48 -0500

    small test

--- assorted_scripts/random_vector_test.py
+++ assorted_scripts/random_vector_test.py
@@ -1,11 +1,30 @@
 import numpy as np
 
 def rand_embedding_generator(vectors):
-    
-    # shape [<num_inputs>,<dimensions>]
+   
+    shape = vectors.shape()
+    print(shape)
+
+ 
+    # shape [<inputs>,<dimensions>]
     rand_emb_array = []
 
-    for i in range(len(embedding_tensor)):
-        vec = np.random.rand(len(embedding_tensor[0]))
+    for i in range(len(vectors)):
+        vec = np.random.rand(len(vectors[0]))
         vec = vec / np.linalg.norm(vec)
         rand_emb_array.append(vec)
+
+
+#========1=========2=========3=========4=========5=========6=========7==
+
+if __name__ == "__main__":
+    # stuff only to run when not called via 'import' here
+    
+    dims = 100
+    inputs = 1000
+
+    vectors = np.random.rand(1000, 100)
+    
+    # Generate. 
+    rand_embedding_generator(vectors)
+
