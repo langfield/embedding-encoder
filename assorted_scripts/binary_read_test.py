8199bd9fed77f5f24ce0d99a523dae8d2dc033ec
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-01-02 21:36:36 -0500

    gensim

--- assorted_scripts/binary_read_test.py
+++ assorted_scripts/binary_read_test.py
@@ -1,5 +1,10 @@
+from gensim.models.keyedvectors import KeyedVectors
+
 import pyemblib
+import gensim
 import os
 
-path = os.path.abspath("~/NER/embeddings/GoogleNews-vectors-negative300.bin")
+path = os.path.abspath("../../embeddings/GoogleNews-vectors-negative300.bin")
+
+gensim_embeddings = KeyedVectors.load_word2vec_format('path/to/GoogleNews-vectors-negative300.bin', binary=True)
 embedding = pyemblib.read(path, format='Word2Vec', mode=pyemblib.Mode.Binary)
